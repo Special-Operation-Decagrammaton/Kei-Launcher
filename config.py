dbfced299c11105d5c893870c002de6c78d15808
ArkanDash <suckgamer0@gmail.com>
2026-09-25 10:37:13 +0700

    feat: 1.3.1

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ LAUNCHER_REPO = "Special-Operation-Decagrammaton/Kei-Launcher"
 R2_URL_EN = "https://pub-5015e736aee54dd4a2455d7e23124ea0.r2.dev"     # Public URL for EN bucket (en-ext / en-ori)
 R2_URL_PTBR = "https://pub-47a39da16fb849bd811f09ec299bd6e8.r2.dev"   # Public URL for PT-BR bucket (pt-br)
 
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 CONFIG_DIR = Path(Path.home()).joinpath('Kei-Launcher')
 CONFIG_PATH = CONFIG_DIR.joinpath('Config.json')
 MANIFEST_PATH = CONFIG_DIR.joinpath('Manifest.json')
